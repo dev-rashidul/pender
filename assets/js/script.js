2d27a6ae5b71e1149c8e29288820ea8ed79fe938
dev-rashidul <rashidul.contact@gmail.com>
2023-01-17 16:06:17 +0600

    Header and Hero Section Done

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Sticky Navbar for Header
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector(".navbar")
+    header.classList.toggle("sticky", window.scrollY > 0)
+  })
+  
