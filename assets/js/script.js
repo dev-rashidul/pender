847d5a8df145db6df6d42936c02a5b1e10ee0ffc
dev-rashidul <rashidul.contact@gmail.com>
2023-01-21 16:17:49 +0600

    User Page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,12 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector(".navbar")
     header.classList.toggle("sticky", window.scrollY > 0)
   })
-  
+
+// JavaScript for Edit Profile
+
+const editIcon = document.querySelector('.user-profile-icon')
+const profileDropdown = document.querySelector('.profile-dropdown')
+
+editIcon.addEventListener("click", function(){
+  profileDropdown.classList.toggle('active')
+})
