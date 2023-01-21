b7dbe0c4e2289331461f8e64ccff8daebda75693
dev-rashidul <rashidul.contact@gmail.com>
2023-01-21 17:22:02 +0600

    Modal Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,25 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0)
   })
 
-// JavaScript for Edit Profile
+// JavaScript for View Profile
 
 const editIcon = document.querySelector('.user-profile-icon')
 const profileDropdown = document.querySelector('.profile-dropdown')
 
 editIcon.addEventListener("click", function(){
   profileDropdown.classList.toggle('active')
+})
+
+// JavaScript for Edit Profile
+
+const userEditIcon = document.querySelector('.user-edit-icon')
+const closeIcon = document.querySelector('.form-close-btn')
+const userModal = document.querySelector('.user-modal')
+
+userEditIcon.addEventListener("click", function(){
+  userModal.classList.toggle('active')
+})
+
+closeIcon.addEventListener("click", function(){
+  userModal.classList.remove('active')
 })
