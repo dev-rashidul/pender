e187f68ca6449de51d1a3a040bcbb4d80525fabd
dev-rashidul <rashidul.contact@gmail.com>
2023-02-11 18:54:00 +0600

    Profile page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
 // Sticky Navbar for Header
 
-window.addEventListener("scroll", function(){
-    var header = document.querySelector(".navbar")
-    header.classList.toggle("sticky", window.scrollY > 0)
-  })
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".navbar");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 // JavaScript for View Profile
 
-const editIcon = document.querySelector('.user-profile-icon')
-const profileDropdown = document.querySelector('.profile-dropdown')
+const editIcon = document.querySelector(".user-profile-icon");
+const profileDropdown = document.querySelector(".profile-dropdown");
 
-editIcon.addEventListener("click", function(){
-  profileDropdown.classList.toggle('active')
-})
+editIcon.addEventListener("click", function () {
+  profileDropdown.classList.toggle("active");
+});
 
 // JavaScript for Edit Profile
 
-const userEditIcon = document.querySelector('.user-edit-icon')
-const closeIcon = document.querySelector('.form-close-btn')
-const userModal = document.querySelector('.user-modal')
+const userEditIcon = document.querySelector(".user-edit-icon");
+const closeIcon = document.querySelector(".form-close-btn");
+const userModal = document.querySelector(".user-modal");
 
-userEditIcon.addEventListener("click", function(){
-  userModal.classList.toggle('active')
-})
+userEditIcon.addEventListener("click", function () {
+  userModal.classList.toggle("active");
+});
 
-closeIcon.addEventListener("click", function(){
-  userModal.classList.remove('active')
-})
+closeIcon.addEventListener("click", function () {
+  userModal.classList.remove("active");
+});
